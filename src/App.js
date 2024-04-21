61f6d535940639d45b4e28d00af79c206f6dd125
bsdaoquang <bsdaoquang@gmail.com>
2024-04-21 20:14:14 +0700

    fix deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 /** @format */
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	console.log(process.env.REACT_APP_MY_KEY);
-
 	return (
 		<div className='App'>
 			<h1>Hello word</h1>
